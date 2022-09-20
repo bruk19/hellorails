8a818e29fb886c43900d70a6139f77a325fe5842
bruk19 <brukteshome21@gmail.com>
2022-09-20 16:26:31 +0300

    Add required changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "pages#hello"
 
-  get "/pages", to: "pages#hello"
+  get '/greeting', to: 'custom_pages#hello'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
